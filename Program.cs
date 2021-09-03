527e80a85c04f3a07a402bf80b5740505e3c3dce
ernane <ernanecarvalho89@gmail.com>
2021-09-02 21:53:01 -0300

    Start of project with Dapper for access data

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void ReadUsers()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                
+            }
+        }
+    }
+}
