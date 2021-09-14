478fc5f15926c71d1d75c8393b363319102acb3e
ernane <ernanecarvalho89@gmail.com>
2021-09-13 22:40:55 -0300

    Finish class model and initial Repository

--- Program.cs
+++ Program.cs
@@ -1,20 +1,43 @@
-﻿using System;
+using System;
+using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
 
 namespace Blog
 {
     class Program
     {
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+
         static void Main(string[] args)
         {
-            
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            ReadUsers(connection);
+            ReadRoles(connection);
+            // ReadUser();
+           // CreateUser();
+           // UpdateUser();
+           // DeleteUser();
+            connection.Close();
         }
-        public static void ReadUsers()
+
+        public static void ReadUsers(SqlConnection connection)
         {
-            using(var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                
-            }
-        }
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+
+            foreach(var user in users)
+                Console.WriteLine(user.Name);
+        } 
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+
+            foreach(var role in roles)
+                Console.WriteLine(role.Name);
+        }       
     }
 }
